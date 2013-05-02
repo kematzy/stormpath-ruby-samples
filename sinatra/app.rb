40a1a4603a3beefaf61895128627dff0dfbd8c12
Erin Swenson-Healey <erin@carbonfive.com>
2013-05-02 15:08:15 -0700

    Sinatra app can now handle inbound requests to validate pw reset
    - Updating "password_reset" to "password_reset_tokens"
    
    - Will accept a token, submit to Stormpath for verification, and upon success render a pw reset form

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -121,11 +121,17 @@ delete "/session" do
   redirect "/"
 end
 
-get '/password_reset/new' do
+get '/password_reset_tokens/new' do
   render_view :password_reset_new
 end
 
-post '/password_reset' do
+get '/password_reset_tokens' do
+  account = application.verify_password_reset_token params[:sptoken]
+
+  redirect "/password_reset_tokens/#{CGI.escape(account.href)}"
+end
+
+post '/password_reset_tokens' do
   begin
     application.send_password_reset_email params[:email]
     redirect '/session/new'
@@ -135,3 +141,18 @@ post '/password_reset' do
     }
   end
 end
+
+get "/password_reset_tokens/:account_url" do
+  account = client.accounts.get CGI.unescape(params[:account_url])
+
+  render_view :password_reset_tokens_edit, { :account => account }
+end
+
+patch '/password_reset_tokens/:account_url' do
+  account = client.accounts.get CGI.unescape(params[:account_url])
+  account.password = params[:password]
+  account.save
+
+  redirect '/session/new'
+end
+
