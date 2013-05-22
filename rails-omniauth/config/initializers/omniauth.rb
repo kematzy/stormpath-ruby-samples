90e625fa347365943b5039e8c0734c1e7374fc27
Erin Swenson-Healey <erin@carbonfive.com>
2013-05-22 15:15:44 -0700

    Take advantage of new config via Rack setup

--- rails-omniauth/config/initializers/omniauth.rb
+++ rails-omniauth/config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :stormpath, '/session/new'
+  provider :stormpath, :setup => lambda { |env|
+    env['omniauth.strategy'].options[:auth_redirect] = '/session/new'
+    env['omniauth.strategy'].options[:authenticator_class] = ::User
+    env['omniauth.strategy'].options[:obtain_uid] = Proc.new { |o| o.stormpath_url }
+  }
 end
