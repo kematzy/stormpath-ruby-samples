ec5ff0cf6aab6a226caaa88113d0d157b162295d
Erin Swenson-Healey <erin@carbonfive.com>
2013-05-23 11:46:12 -0700

    Replacing form_for with form_tag
    - using form_for caused the Rack::Request to be in a format unsuitable for our OmniAuth strategy

--- rails-omniauth/config/initializers/omniauth.rb
+++ rails-omniauth/config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :stormpath, :setup => lambda { |env|
     env['omniauth.strategy'].options[:auth_redirect] = '/session/new'
-    env['omniauth.strategy'].options[:authenticator_class] = ::User
-    env['omniauth.strategy'].options[:obtain_uid] = Proc.new { |o| o.stormpath_url }
+    env['omniauth.strategy'].options[:authenticator_method] = ::User.method(:authenticate)
+    env['omniauth.strategy'].options[:obtain_uid_method] = Proc.new { |o| o.stormpath_url }
   }
 end
