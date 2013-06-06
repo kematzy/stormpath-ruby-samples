2fba779e1b844b18a9c1c0d6fc02bdf2133f86f5
Boram Yoon <boram@carbonfive.com>
2013-06-06 14:39:42 -0700

    Clean up specs, add vcr to rails-omniauth

--- rails-omniauth/spec/features/sessions_spec.rb
+++ rails-omniauth/spec/features/sessions_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Authentication" do
+describe "Authentication", :vcr do
   email = "test#{SecureRandom.hex}@example.com"
   password = 'Succ3ss!'
 
-  before(:all) do
+  before do
     @user = User.create!(
       email: email,
       password: password,
@@ -14,62 +14,62 @@ describe "Authentication" do
     )
   end
 
-  after(:all) do
+  after do
     @user.destroy
   end
 
   describe "Sign in" do
     before do
       visit new_session_path
-      fill_in "email_or_username", with: email
+      fill_in 'email_or_username', with: email
     end
 
-    describe "with valid credentials" do
+    describe 'with valid credentials' do
       before do
-        fill_in "password", with: password
-        click_button "Sign in"
+        fill_in 'password', with: password
+        click_button 'Sign in'
       end
 
-      it "authenticates valid email and password" do
-        expect(page).to have_content("Signed in")
+      it 'authenticates valid email and password' do
+        expect(page).to have_content "Signed in"
       end
 
-      it "has signed in user info" do
-        expect(page).to have_content("Signed in as: #{email}")
+      it 'has signed in user info' do
+        expect(page).to have_content "Signed in as: #{email}"
       end
     end
 
-    describe "without valid credentials" do
+    describe 'without valid credentials' do
       before do
-        fill_in "password", with: "wrongpassword"
-        click_button "Sign in"
+        fill_in 'password', with: 'wrongpassword'
+        click_button 'Sign in'
       end
 
-      it "fails authentication with invalid password" do
-        expect(page).to have_content("Authentication failed")
+      it 'fails authentication with invalid password' do
+        expect(page).to have_content 'Authentication failed'
       end
 
-      it "does not have signed in user info" do
-        expect(page).to_not have_content("Signed in as: #{email}")
+      it 'does not have signed in user info' do
+        expect(page).to_not have_content "Signed in as: #{email}"
       end
     end
   end
 
-  describe "Sign out" do
+  describe 'Sign out' do
     before do
       visit new_session_path
-      fill_in "email_or_username", with: email
-      fill_in "password", with: password
-      click_button "Sign in"
-      click_link "Sign out"
+      fill_in 'email_or_username', with: email
+      fill_in 'password', with: password
+      click_button 'Sign in'
+      click_link 'Sign out'
     end
 
-    it "acknowledges signing out" do
-      expect(page).to have_content("Signed out")
+    it 'acknowledges signing out' do
+      expect(page).to have_content 'Signed out'
     end
 
-    it "does not have signed in user info" do
-      expect(page).to_not have_content("Signed in as: #{password}")
+    it 'does not have signed in user info' do
+      expect(page).to_not have_content "Signed in as: #{password}"
     end
   end
 end
