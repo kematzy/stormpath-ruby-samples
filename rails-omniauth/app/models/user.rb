46500166c78af0c1846fb8f620aaab05daaaadd9
Frank Caron <frank@frankcaron.com>
2013-08-17 00:45:42 -0700

    Fix for user object missing error
    Resolves the missing user object error which prevented the sample app
    from working

--- rails-omniauth/app/models/user.rb
+++ rails-omniauth/app/models/user.rb
@@ -3,9 +3,9 @@ require 'stormpath-rails'
 class User < ActiveRecord::Base
   include Stormpath::Rails::Account
 
-  attr_accessible :email, :password, :given_name, :surname, :username
+  attr_accessible :email, :password, :given_name, :surname, :username, :stormpath_url
 
   def self.from_omniauth(auth)
-    where(stormpath_url: auth["uid"]).first
+    where(stormpath_url: auth["uid"]).first_or_create
   end
 end
